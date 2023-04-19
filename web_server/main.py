5e8c979489e1bd4015d7d396301dddfd6b89c67e
Fernando <fernando268a@gmail.com>
2023-04-19 18:25:35 -0400

    web_server fastIPA

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+@app.get("/")
+def get_inf():
+    return [1,2,3]
+
+@app.get("/inf")
+def get_inf():
+    return {"name": "FastAPI"}
+
+@app.get("/home", response_class=HTMLResponse)
+def get_home():
+    return "<h1>Hello World</h1>"
+
 
 def run():
     store.get_categories()
--- web_server/store.py
+++ web_server/store.py
@@ -4,8 +4,11 @@ def get_categories():
     res = requests.get("https://api.escuelajs.co/api/v1/categories")
     print(res.status_code)
     # print(res.text)
+    print(type(res))
 
     categories = res.json()
+    print(categories)
+    print(type(categories))
     for category in categories:
         print(category["name"])
 
