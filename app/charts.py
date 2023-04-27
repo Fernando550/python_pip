acedd2bf1b8c75a8a7d008b0f31b307eb94825e6
Fernando <fernando268a@gmail.com>
2023-04-27 14:15:57 -0400

    Docker containers

--- app/charts.py
+++ app/charts.py
@@ -13,7 +13,7 @@ def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   plt.axis("equal")
-  plt.savefig("pie.png")
+  plt.savefig("pie2.png")
   plt.close()
 
 if __name__ == "__main__":
